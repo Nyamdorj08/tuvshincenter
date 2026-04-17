f7613e884ca410c128992392bfb269e11eea23ba
Nyamdorj08 <nyamdorjnika@gmail.com>
2026-04-17 14:49:15 +0800

    feat: initial commit

---
+++ src/app/section/Homeview.tsx
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import Product from "./Product";
+import Info from "./Info";
+import Faq from "./Faq";
+import Review from "./Review";
+import Forum from "./Forum";
+import Footer from "./Footer";
+
+export default function Homeview() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Product />
+      <Info />
+      <Faq />
+      <Review />
+      <Forum />
+      <Footer />
+    </>
+  );
+}
